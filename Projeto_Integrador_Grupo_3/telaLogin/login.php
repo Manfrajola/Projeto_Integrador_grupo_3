aaa0357032522700b70d30c539226b1ffd2440dc
Jair Alves <112443175+Manfrajola@users.noreply.github.com>
2022-11-02 21:44:18 -0300

    Add files via upload

---
+++ Projeto_Integrador_Grupo_3/telaLogin/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" style="background-color: #90CAF9">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+  
+  <div class="login">
+     <img class="logo" src="img/luli2.png" alt="logo">
+     <div class="email">
+        <img class="icons" src="img/key.svg" alt="">
+        <input class="campo" type="text" name="email" placeholder="E-mail"><p></p>
+      </div>
+      <div class="senha" >
+        <img class="icons" src="img/padlock.svg" alt="">
+        <input class="campo" type="password" name="senha" placeholder="senha"><p></p>
+      </div>
+      <div class="info">
+       <a href="">Nao tem uma conta? Clique aqui</a><br>
+       <a href="">Esqueceu a senha? Clique aqui</a>
+      </div>
+    <div class="btn">
+      <button type ="button" class="submit">Login</button>
+    
+    </div>
+  </div>
+    <div class="gif">
+    <img src="img/Computer login.gif" alt="">
+    </div>
+  
+    
+</body>
+</html>
